17be592b4829223fecddd3b7da503f555b3b76b5
shivangupadhyy <shivangupadhyay07@gmail.com>
2025-05-17 23:52:11 +0530

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js code as newer version
+
+// alert("Hello Shivang");
+// alert(3+3);  we are using nodejs , not browser
+
+// code readability should be high
+// use camelCase for variables
+
+let name = "Shivang";
+let age  = 20;
+let isLoggedIn = false;
+
+
+// number =>2 to power 53
+//bigint 
+//string = ""
+//boolean => true/false
+//null = standalone value
+//undefined =>not assigned
+//symbol => unique
+
+//object => collection of key value pairs
+//object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
