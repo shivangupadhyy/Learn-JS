3e7b659d0c8a8503b7fa99533cb206dbd761557b
shivangupadhyy <shivangupadhyay07@gmail.com>
2025-05-18 17:32:51 +0530

    conversionOperation and comparision

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -12,9 +12,9 @@
 // "121ab" => NaN
 //true => 1 ;  false => 0
 
-let isLoggedIn = 1
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn); //true
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn); //true
 
 
 // 0 => false
@@ -23,7 +23,65 @@ console.log(booleanIsLoggedIn); //true
 // 1 => true
 // "shivang" => true
 
-let someNumber = 122;
-let stringNumber = String(someNumber);
-console.log(stringNumber); 
-console.log(typeof stringNumber); //string
+// let someNumber = 122;
+// let stringNumber = String(someNumber);
+// console.log(stringNumber); 
+// console.log(typeof stringNumber); //string
+// **************** operation ***************
+
+// let value = 4;
+// let negValue = -value 
+// console.log(negValue); 
+
+
+// console.log(2+2);//4
+// console.log(2-2);//0
+// console.log(2*2);//4
+// console.log(2/2);//1
+// console.log(2**2);//4
+// console.log(2%2);//0
+// console.log(2**3); //8
+
+// let str1 = "hello";
+// let str2 = " shivang";
+
+// let str= str1 + str2;
+// console.log(str); //helloshivang
+
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+// console.log(1 + 2 + "3"); //33
+// console.log("2" + 1 + 2); //212
+
+// console.log(+true); 
+// console.log(+"");
+
+
+// let num1, num2, num3;
+
+// num1 = num2 = num3 = 2 + 3; //5
+
+// let gameCounter = 100;
+// ++gameCounter;
+
+// console.log(gameCounter); //101
+
+
+//post increment
+
+let x = 4;
+let y = x++; 
+console.log(x);
+console.log(y); 
+
+//pre increment
+let a = 2;
+let b = ++a;
+console.log(a);
+console.log(b); 
+
+
+
+
+
+ 
