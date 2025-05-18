56076bcd64e1ad0122310f07314cbbc89c52311e
shivangupadhyy <shivangupadhyay07@gmail.com>
2025-05-18 11:27:43 +0530

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+// let score = undefined; //undefined
+
+// console.log(typeof score);
+// console.log( typeof (score));
+ 
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "121ab" => NaN
+//true => 1 ;  false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn); //true
+
+
+// 0 => false
+// "" => false
+// null => false
+// 1 => true
+// "shivang" => true
+
+let someNumber = 122;
+let stringNumber = String(someNumber);
+console.log(stringNumber); 
+console.log(typeof stringNumber); //string
