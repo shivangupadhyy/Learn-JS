3a20c797b1fb0a2b5809f6aea042dece121345e5
shivangupadhyy <shivangupadhyay07@gmail.com>
2025-06-07 23:44:50 +0530

    few more topics

---
+++ 10_classes_and_oop/mathpi.js
@@ -0,0 +1,38 @@
+const descripter = Object.getOwnPropertyDescriptor(Math, "PI")
+
+// console.log(descripter)
+
+console.log(Math.PI)
+
+// const myNewObj = Object.create(null)
+
+const chai = {
+    name:"ginger chai",
+    price: 250,
+    isAvaiable : true,
+
+    orderchai : function(){
+        console.log(`chai nhi baane`)
+    }
+}
+
+console.log(chai)
+console.log(Object.getOwnPropertyDescriptor(chai, "name"))
+
+// Object.defineProperty(chai, "name",{
+//     writable: false,
+//     enumerable: false
+// })
+
+
+// console.log(Object.getOwnPropertyDescriptor(chai, "name"))
+
+for (let [key, value] of Object.entries(chai)) {
+    if(typeof value !== 'function'){
+        console.log(`${key} : ${value}`)
+
+    }
+
+    
+
+}
